ad3896175515b500d214b4dfb32cd604bcf39bf6
be-ausome <dave@be-ausome.com>
2025-09-13 12:50:56 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head />
-      <body className="min-h-screen bg-white text-black">{children}</body>
+      <body className="min-h-screen antialiased">
+        {children}
+      </body>
     </html>
-  );
+  )
 }
